50179b6f87089e825c4ad20e788ba1ebf8fc8de8
kayla181107 <kaylawulandari863@gmail.com>
2025-09-18 17:43:53 +0700

    fist push

---
+++ app/Services/Auth/RegisterService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services\Auth;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class RegisterService
+{
+    public function register(array $data): array
+{
+
+    $user = User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+    ]);
+
+    $token = $user->createToken('TokenLogin')->accessToken;
+
+    return [
+        'user' => $user,
+        'token' => $token,
+    ];
+}
+
+}
