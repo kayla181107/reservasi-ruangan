00ec8af643531ca8fc1da82823495a7a797fc247
kayla181107 <kaylawulandari863@gmail.com>
2025-09-21 21:26:37 +0700

    menambahkan file di folder resources

---
+++ app/Http/Resources/FixedScheduleResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FixedScheduleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'          => $this->id,
+            'room'        => new RoomResource($this->whenLoaded('room')),
+            'day_of_week' => $this->day_of_week,
+            'start_time'  => $this->start_time,
+            'end_time'    => $this->end_time,
+            'description' => $this->description,
+            'created_at'  => $this->created_at ? $this->created_at->toDateTimeString() : null,
+            'updated_at'  => $this->updated_at ? $this->updated_at->toDateTimeString() : null,
+        ];
+    }
+}
---
+++ app/Http/Resources/ReservationResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id,
+            'user'       => $this->whenLoaded('user', function () {
+                return [
+                    'id'    => $this->user->id,
+                    'name'  => $this->user->name,
+                    'email' => $this->user->email,
+                ];
+            }),
+            'room'       => new RoomResource($this->whenLoaded('room')),
+            'date'       => $this->date,
+            'start_time' => $this->start_time,
+            'end_time'   => $this->end_time,
+            'status'     => $this->status,
+            'reason'     => $this->reason,
+            'created_at' => $this->created_at ? $this->created_at->toDateTimeString() : null,
+            'updated_at' => $this->updated_at ? $this->updated_at->toDateTimeString() : null,
+        ];
+    }
+}
